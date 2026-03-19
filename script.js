be5c59c93ec3d080b6fe2def141e72881e9f846c
AndereX-dev <andreas.osdal@hotmail.com>
2026-03-19 13:17:52 +0100

    changed up the CSS, HTML-files and added a reset-button with a little more styling on the heading, cards and button

---
+++ script.js
@@ -0,0 +1,104 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll("[data-view='card']");
+  const secondsHtml = document.getElementById("seconds");
+  const tenthsHtml = document.getElementById("tenths");
+  const resetBtn = document.getElementById("reset-btn");
+
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let firstCard, secondCard;
+  let seconds = 0;
+  let tenths = 0;
+  let interval;
+
+  function flipcard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add("flipped");
+
+    if (!hasFlippedCard) {
+      hasFlippedCard = true;
+      firstCard = this;
+      startTimer();
+      return;
+    }
+    secondCard = this;
+    checkForMatch();
+  }
+
+  function checkForMatch() {
+    let isMatch = firstCard.dataset.item === secondCard.dataset.item;
+    isMatch ? disableCards() : unflipCards();
+  }
+
+  function disableCards() {
+    firstCard.classList.add("correct");
+    secondCard.classList.add("correct");
+
+    resetBoard();
+    checkWin();
+  }
+
+  function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+      firstCard.classList.remove("flipped");
+      secondCard.classList.remove("flipped");
+      resetBoard();
+    }, 1000);
+  }
+
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+  function shuffle() {
+    cards.forEach((card) => {
+      let randomPos = Math.floor(Math.random() * 20);
+      card.style.order = randomPos;
+    });
+  }
+
+  function startTimer() {
+    if (interval) return;
+    interval = setInterval(() => {
+      tenths++;
+      if (tenths > 99) {
+        seconds++;
+        tenths = 0;
+      }
+      secondsHtml.innerHTML = seconds < 10 ? "0" + seconds : seconds;
+      tenthsHtml.innerHTML = tenths < 10 ? "0" + tenths : tenths;
+    }, 10);
+  }
+
+  function stopTimer() {
+    clearInterval(interval);
+    interval = null;
+  }
+
+  function checkWin() {
+    const allCorrect = document.querySelectorAll(".correct").length;
+    if (allCorrect === cards.length) {
+      stopTimer();
+      alert(`Congratulation! You used ${seconds}:${tenths} second's`);
+    }
+  }
+
+  resetBtn.addEventListener("click", () => {
+    stopTimer();
+    seconds = 0;
+    tenths = 0;
+    secondsHtml.innerHTML = "00";
+    tenthsHtml.innerHTML = "00";
+
+    cards.forEach((card) => {
+      card.classList.remove("flipped", "correct");
+    });
+
+    setTimeout;
+  });
+});
